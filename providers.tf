af5561adf57a9b176f57c22dcfdeb8e672ad22c9
Joshua Geissler <jgeissler14@gmail.com>
2025-12-19 13:56:42 -0800

    InstancePrincipal

--- providers.tf
+++ providers.tf
@@ -8,6 +8,5 @@ terraform {
 }
 
 provider "oci" {
-  # See https://registry.terraform.io/providers/oracle/oci/latest/docs/guides/provider_configuration
-  # for more information on how to configure the provider
+  auth = "InstancePrincipal"
 }
